379a0c0314ccba63ec3b29c6d091e6c7a95ea8b0
Jyles Coad-Ward <jyles@dariox.club>
2021-03-05 14:30:38 +0800

    for fuck sake

--- tools/async.js
+++ tools/async.js
@@ -1,9 +1,9 @@
 module.exports = 
 {
-	forEach: async(arr,cb) =>
+	forEach: async(arr,callback) =>
 	{
 		if (arr === undefined) throw "Array is undefined";
-		if (cb  === undefined) throw "Callback is not defined";
+		if (callback  === undefined) throw "Callback is not defined";
 		for (let index = 0; index < arr.length; index++) {
 			await callback(arr[index], index, arr)
 		}
