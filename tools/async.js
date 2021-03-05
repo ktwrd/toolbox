84c15865b2cdbb0140c8716101df0ed5e8732af9
Jyles Coad-Ward <jyles@dariox.club>
2021-03-05 14:29:03 +0800

    bugfix

--- tools/async.js
+++ tools/async.js
@@ -4,13 +4,8 @@ module.exports =
 	{
 		if (arr === undefined) throw "Array is undefined";
 		if (cb  === undefined) throw "Callback is not defined";
-<<<<<<< HEAD
 		for (let index = 0; index < arr.length; index++) {
 			await callback(arr[index], index, arr)
-=======
-		for (let index = 0; index < array.length; index++) {
-			await callback(array[index], index, array)
->>>>>>> d855eeed84480bc8049d65df6e1d6c464606c21c
 		}
 	}
 }
